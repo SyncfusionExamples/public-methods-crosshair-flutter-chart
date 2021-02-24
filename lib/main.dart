4f103de2ff486684085e944a698e48c227699196
Pavithra Nandhini Ramachandran <pavithranandhini.ramachandran@syncfusion.com>
2021-02-24 10:23:49 +0530

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ class ChartApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Chart Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
     );
   }
@@ -30,68 +28,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  SfCartesianChart chart;
+  CrosshairBehavior _crosshairBehavior;
 
-SfCartesianChart chart;
-CrosshairBehavior crosshair;
-
+  @override
+  void initState() {
+    _crosshairBehavior = CrosshairBehavior(enable: true);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final List<ChartData> chartData = [
-    ChartData(10, 17),
-    ChartData(20, 34),
-    ChartData(30, 25),
-    ChartData(40, 10),
-    ChartData(50, 10)
-// Add the required data  
-];
-crosshair = CrosshairBehavior(enable: true);
- chart = SfCartesianChart(
-
-   backgroundColor: Colors.white,
-   primaryXAxis: NumericAxis(interval: 10),
-    crosshairBehavior: crosshair,
-  series: <CartesianSeries>[
-    StepLineSeries<ChartData, double>(
-        enableTooltip: true,
-        dataSource: chartData,
-        xValueMapper: (ChartData data, _) => data.x,
-        yValueMapper: (ChartData data, _) => data.y),
-    ]
-);
-return Scaffold(
-  appBar: AppBar(
-    title: Text('Crosshair')
-  ),
-  body: Container(
-    child: Column(
-      children: <Widget>[
-        FlatButton(
-          color: Theme.of(context).primaryColor,
-          child: Text('Show'),
-          onPressed: show,
-
-        ),
-        Center(
-          child: Container(height: 500,
-          width: 320,
-            child: chart),
-        )
-      ]
-    )
-  )
-);
-
+      ChartData(10, 17),
+      ChartData(20, 34),
+      ChartData(30, 25),
+      ChartData(40, 10),
+      ChartData(50, 10)
+// Add the required data
+    ];
+    chart = SfCartesianChart(
+        backgroundColor: Colors.white,
+        primaryXAxis: NumericAxis(interval: 10),
+        crosshairBehavior: _crosshairBehavior,
+        series: <CartesianSeries>[
+          StepLineSeries<ChartData, double>(
+              enableTooltip: true,
+              dataSource: chartData,
+              xValueMapper: (ChartData data, _) => data.x,
+              yValueMapper: (ChartData data, _) => data.y),
+        ]);
+    return Scaffold(
+        appBar: AppBar(title: Text('Crosshair')),
+        body: Container(
+            child: Column(children: <Widget>[
+          FlatButton(
+            color: Theme.of(context).primaryColor,
+            child: Text('Show'),
+            onPressed: show,
+          ),
+          Center(
+            child: Container(height: 500, width: 320, child: chart),
+          )
+        ])));
   }
 
-void show() {
-  crosshair.showByIndex(2);
+  void show() {
+    _crosshairBehavior.showByIndex(2);
   }
 
-void hide(){
-    crosshair.hide();
-}
-
+  void hide() {
+    _crosshairBehavior.hide();
+  }
 }
 
 class ChartData {
